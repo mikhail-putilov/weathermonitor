139c44a76c3cc3edee7b6b3c0c81250f2c2ec7ae
Putilov Michael <post.snowy@gmail.com>
2015-09-23 15:17:57 +0500

    Initial commit

---
+++ src/main/java/io/github/musius/WeatherMonitorApplication.java
@@ -0,0 +1,12 @@
+package io.github.musius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherMonitorApplication.class, args);
+    }
+}
