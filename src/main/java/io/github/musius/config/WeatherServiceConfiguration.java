8cf7cfb7ab3504679588ba6b4c3a3a1267ade692
Putilov Michael <post.snowy@gmail.com>
2015-09-24 17:02:05 +0500

    fix injecting cities

--- src/main/java/io/github/musius/config/WeatherServiceConfiguration.java
+++ src/main/java/io/github/musius/config/WeatherServiceConfiguration.java
@@ -1,29 +1,14 @@
 package io.github.musius.config;
 
-import com.google.common.collect.Lists;
-import io.github.musius.service.util.WeatherCrawler;
-import io.github.musius.service.util.YahooWeatherCrawler;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Configuration
 public class WeatherServiceConfiguration {
-    /**
-     * Хак, из-за странного бага:
-     * {@code @Value("${listOfStrings}") List<String> lst;} не обрабатывается корректно
-     */
-    @Bean
-    public List<String> cities(@Value("${service.weather.cities[0]}") String city1, @Value("${service.weather.cities[1]}") String city2) {
-        return Lists.newArrayList(city1, city2);
-    }
-
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
 }
--- src/main/java/io/github/musius/service/WeatherCrawlerService.java
+++ src/main/java/io/github/musius/service/WeatherCrawlerService.java
@@ -7,27 +7,30 @@ import io.github.musius.service.util.WeatherCrawler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 
+@ConfigurationProperties(prefix = "service.weather")
 @Service
 public class WeatherCrawlerService {
     private final Logger log = LoggerFactory.getLogger(WeatherCrawlerService.class);
 
     @Autowired
     List<WeatherCrawler> crawlers;
-    @Resource
-    List<String> cities;
+
+    private List<String> cities = new ArrayList<>(); //from properties
+
     @Inject
     WeatherDataRepository repo;
 
     public void populateDatabaseWithNewWeatherInformation() {
         List<WeatherData> toBeSaved = Lists.newArrayList();
-        for (String city : cities) {
+        for (String city : getCities()) {
             for (WeatherCrawler crawler : crawlers) {
                 tryCrawlDataOrSilence(toBeSaved, city, crawler);
             }
@@ -47,5 +50,9 @@ public class WeatherCrawlerService {
     @Scheduled(fixedDelayString = "${service.weather.fixed_delay_seconds}000")
     void autoPopulateDatabaseWithFixedDelay() {
         populateDatabaseWithNewWeatherInformation();
+    }
+
+    public List<String> getCities() {
+        return cities;
     }
 }
